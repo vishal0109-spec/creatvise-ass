4461943297b9dba95cf415fd67c8e85ff9c71547
Vishal <vishal01092@gmail.com>
2024-01-03 00:05:07 +0530

    changes in buttons bg

--- src/components/Property1Group6981.js
+++ src/components/Property1Group6981.js
@@ -19,7 +19,7 @@ const Property1Group6981 = ({
       style={property1Group6981Style}
     >
       <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-31xl bg-slateblue" />
-      <div className="absolute top-[23.81%] left-[20.2%] tracking-[-0.05em] leading-[150%] font-semibold">
+      <div className="absolute p-2 m-2 top-[23.81%] left-[20.2%] tracking-[-0.05em] leading-[150%]  bg-blue-900 font-semibold">
         Get Started
       </div>
     </div>
